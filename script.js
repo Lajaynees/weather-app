66c5fc513bb70f7d53ea9a88f88b36328f8c843d
Lajaynees <lscingram@yahoo.com>
2021-10-19 12:30:58 -0400

    added getData-fixed saved localStorage-added TempEL-app working

--- script.js
+++ script.js
@@ -1,35 +1,90 @@
-var apiKey="1084c55f9f5c13f04e61f51385b523c6"
-var submitBtn=document.querySelector("#searchbtn")
-//var cityName= document.querySelector("#search_bar")
-//Submit button
-//event listner
-//input for information
-//fetch using Api and input
-//if statements check the data
+var apiKey = "1084c55f9f5c13f04e61f51385b523c6"
+var submitBtn = document.querySelector("#searchbtn")
+var cityName = document.querySelector("name_of_city");
+var buttonEl = document.querySelector(".btn");
+var fiveDayForecast = document.querySelector("five-day-div");
+var formEl = document.querySelector("#form");
+var currentTemp = document.querySelector("today_temp");
+var currentHumidity = document.querySelector("today_humidity");
+var currentWindSpeed = document.querySelector("today_wind_speed");
+var cities = [];
+
+function savedCities(cityName){
+
+console.log(cityName)
 
 
-
-
-function getWeather(){
-var cityName= document.querySelector("#search_bar").value
-var weatherAPI="https://api.openweathermap.org/data/2.5/weather?q="+ cityName+"&appid="+apiKey
-fetch (weatherAPI)
-.then(function(res){
-  return res.json()
-})
-.then(function(data){
-  console.log(data)
-  getData(data.coord.lat,data.coord.lon)
-//use data to add info to the screen
-})
-.catch(function(err){
-console.log(err)
-})
+var saved = JSON.parse(localStorage.getItem("saved")) || [];
+if (localStorage.getItem("saved")) {
+  saved = JSON.parse(localStorage.getItem("saved"));
 }
-function getData(lat,lon){
-  fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={API key}")
+if (!saved.includes(cityName)) {
+  saved.push(cityName);
+  localStorage.setItem("saved", JSON.stringify(saved));
+}
+}
+function displaySavedCities(){
+  var saved = JSON.parse(localStorage.getItem("saved")) || [];
+for(let i = 0; i < saved.length; i++){
+ let city = saved[i]
+let container = document.getElementById("searched_cities_container")
+let cityEl =document.createElement("button")
+cityEl.textContent = city
+cityEl.addEventListener("click",function(){
+console.log("hello")
+})
+container.appendChild(cityEl)
 }
 
 
-submitBtn.addEventListener("click",getWeather)
+}
+displaySavedCities()
+
+// display weather
+  function getWeather() {
+    var cityName = document.querySelector("#search_bar").value
+    var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+    
+    //fetch using Api and input
+    fetch(weatherAPI)
+      .then(function (res) {
+        return res.json()
+      })
+      .then(function (data) {
+        console.log(data)
+        getData(data.coord.lat, data.coord.lon, data.name)
+        //use data to add info to the screen
+
+
+
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+  }
+
+  // display data
+  function getData(lat, lon, name) {
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
+    .then(function (res) {
+      return res.json()
+    })
+    .then(function (data) {
+      console.log(data)
+      savedCities(name);
+      
+      var tempEl = document.getElementById("today_temp")
+tempEl.textContent = "Temperature: " + data.current.temp + "*F"
+
+
+
+// Loop for 5 day forecast
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+  }
+
+  //event listner
+  submitBtn.addEventListener("click", getWeather)
 //display the Api info(how to go about display)
